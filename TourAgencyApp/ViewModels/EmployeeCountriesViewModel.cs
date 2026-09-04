e66f0c06a6b25b72d74195b46e450c1f5d28b373
Sttrebery <sttrebery@mail.ru>
2026-09-04 09:42:42 +0400

    update: CountriesPage.xaml and its ViewModel are done and working succesfully

--- TourAgencyApp/ViewModels/EmployeeCountriesViewModel.cs
+++ TourAgencyApp/ViewModels/EmployeeCountriesViewModel.cs
@@ -1,16 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using TourAgencyApp.Services;
-using TourAgencyApp.Models;
 using System.Windows;
-using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using TourAgencyApp.Models;
+using TourAgencyApp.Services;
 
 namespace TourAgencyApp.ViewModels
 {
@@ -18,14 +19,22 @@ namespace TourAgencyApp.ViewModels
     {
         private readonly DataService _dataService;
         private ObservableCollection<Country> _countries;
-        private string _countryName;
+        private ICollectionView _countriesView;
+        private string _countryName; //to edit country name
         private string _countryToAdd;
+        private string _countryToRename;
         private bool _isChanged;
 
         public ObservableCollection<Country> Countries
         {
             get => _countries;
             set { _countries = value; OnPropertyChanged(); }
+        }
+
+        public ICollectionView CountriesView
+        {
+            get => _countriesView;
+            set { _countriesView = value; OnPropertyChanged(); }
         }
 
         public string CountryName
@@ -40,17 +49,26 @@ namespace TourAgencyApp.ViewModels
             set { _countryToAdd = value; OnPropertyChanged(); }
         }
 
+        public string CountryToRename
+        {
+            get => _countryToRename;
+            set { _countryToRename = value; OnPropertyChanged(); }
+        }
+
         public ICommand UpdateListCommmand {  get; set; }
         public ICommand AddCountryCommmand {  get; set; }
+        public ICommand RenameCountryCommand { get; set; }
 
         public EmployeeCountriesViewModel(DataService d) 
         {
             _dataService = d;
             _isChanged = false;
             Countries = new ObservableCollection<Country>(_dataService.GetAllCountries()); //получаем либо список, либо пустой List (но не null!)
+            CountriesView = CollectionViewSource.GetDefaultView(Countries);
 
             AddCountryCommmand = new RelayCommand(AddCountry);
             UpdateListCommmand = new AsyncRelayCommand(UpdateList);
+            RenameCountryCommand = new RelayCommand(RenameCountry);
         }
 
         //async
@@ -72,10 +90,35 @@ namespace TourAgencyApp.ViewModels
                 }
             }
             Countries = new ObservableCollection<Country>(await _dataService.GetAllCountriesAsync()); //наоборот - загрузка из БД для обновления ID
+            CountriesView?.Refresh();
+        }
+
+        private void CheckCountryName(string name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Введите новое название!", "Ошибка");
+                return;
+            }
+        }
+
+        private void RenameCountry()
+        {
+            CheckCountryName(CountryName);
+            int countryIndex = Countries.IndexOf(Countries.First(f => f.Name == CountryToRename));
+            Countries[countryIndex].Name = CountryName.Trim();
+            CountryName = string.Empty;
+            CountryToRename = string.Empty;
+            _isChanged = true;
+            MessageBox.Show("Изменения сохранены.");
+
+            CountriesView?.Refresh();
         }
 
         private void AddCountry()
         {
+            CheckCountryName(CountryToAdd);
+
             var founded = Countries.FirstOrDefault(f => f.Name == CountryToAdd);
             if (founded != null)
             {
@@ -84,12 +127,12 @@ namespace TourAgencyApp.ViewModels
             else
             {
                 //добавление в список (отложенный режим)
-                Countries.Add(new Country() { Name = CountryToAdd });
+                Countries.Add(new Country() { Name = CountryToAdd.Trim() });
                 CountryToAdd = string.Empty;
                 _isChanged = true;
                 MessageBox.Show("Страна добавлена!");
 
-                OnPropertyChanged();
+                CountriesView?.Refresh();
             }
         }
 
